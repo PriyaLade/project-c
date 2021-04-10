0aada2ae551ec3fbd862c9fc95f839b027ee338e
PriyaLade <priyalade97@gmail.com>
2021-04-10 20:13:46 +0530

    firbase setup for project

--- src/App.js
+++ src/App.js
@@ -6,16 +6,16 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <h1>
+         Project-EvilC
+        </h1>
         <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Personal Profile
         </a>
       </header>
     </div>
